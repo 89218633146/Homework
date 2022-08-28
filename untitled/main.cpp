7a36f8aa9db8bc1f8651cb4645d285c2f0334919
89218633146 <111647343+89218633146@users.noreply.github.com>
2022-08-28 11:19:11 +0300

    тестовый проект решения уравнений №2

---
+++ untitled/main.cpp
@@ -0,0 +1,19 @@
+#include<math.h>
+#include <iostream>
+
+int main()
+{
+
+    double a = 5, b, c = 5, d;
+    printf("a=a+b-2=%3.1f\n", a = a + b - 2);
+    printf("c=c+1=%3.1f\n", c=c+1);
+    printf("d=c-a+d=%3.1f\n", d = c - a + d);
+    printf("a=a*c=%3.1f\n", a = a * c);
+    printf("c=c-1=%3.1f\n", c = c - 1);
+    printf("a=a/10=%3.1f\n", a = a / 10);
+    printf("c=c/2=%3.1f\n", c = c / 2);
+    printf("b=b-1=%3.1f\n", b = b - 1);
+    printf("d=d*(c+b+a)%3.1f\n", d = d * (c + b +a));
+
+    return 0;
+}
